b329b00fb3f266194695355b0cde69c6b76e476a
Teatoller <steven.otieno1@gmail.com>
2019-01-19 11:49:43 +0300

    feat(auth-social): enable default authentication
    - update rest framework with backend applications
    
    [#162949074]

--- authors/settings.py
+++ authors/settings.py
@@ -172,6 +172,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'authors.apps.authentication.backends.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'oauth2_provider.contrib.rest_framework.OAuth2Authentication',
+        'rest_framework_social_oauth2.authentication.SocialAuthentication',
     ),
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
