e0fdc59a00ce6f1d0809a2ade5e5b95d82da930a
Teatoller <steven.otieno1@gmail.com>
2019-01-30 10:03:52 +0300

    feat(user_notification): notification field
    - custom notification field
    
    [#162949083]

--- authors/apps/notification/models.py
+++ authors/apps/notification/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from ..authentication.models import User
+from ..articles.models import Article
 
-# Create your models here.
+
+class Notification(models.Model):
+    """ """
+    article = models.ForeignKey(
+        Article,
+        blank=True,
+        null=True,
+        on_delete=models.CASCADE
+    )
+
+    user_notification = models.TextField()
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    notified = models.ManyToManyField(
+        User, related_name='notified', blank=True
+    )
+
+    read = models.ManyToManyField(
+        User, related_name='read', blank=True
+    )
+
+    notification_type = models.TextField(default="article")
+
+    def __str__(self):
+        return self.user_notification
