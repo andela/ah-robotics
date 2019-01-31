ac74fa54c2fc0b9de5b7d7dad3bd9cd3f0e91eba
johnwayodi <wayodijohn@gmail.com>
2019-01-31 11:29:09 +0300

    feat(favorites): remove commented code
    - remove commented code block in base_test file
    
    [#162949082]

--- authors/apps/favorites/tests/base_test.py
+++ authors/apps/favorites/tests/base_test.py
@@ -36,19 +36,12 @@ class FavoriteTestBase(APITestCase):
                 "tagList": ["sample", "article"],
                 "author": 1}
         }
-        # self.article_slug = {
-        #     "article_slug": "the-one"
-        # }
-        # self.absent_article_slug = {
-        #     "article_slug": "article-absent"
-        # }
         self.article_slug = "the-one"
         self.non_existent_article_slug = "article-absent"
         self.client = APIClient()
         self.register_url = reverse('authentication:register_url')
         self.login_url = reverse('authentication:login_url')
         self.articles_url = reverse('articles:articles')
-        # self.favorite_url = reverse('favorites:favorites')
         self.all_favorites_url = reverse('favorites:user-favorites')
 
     def register_user(self, data):
