cad86c219490c440779f05dc802bb27c1ebeb20c
Paul Vitalis <vitalispaul48@live.com>
2019-02-01 13:13:14 +0300

    feat(userFollowUnfollow): update unfollow URL
    - update unfollow URL
    
    [#162949081]

--- authors/apps/followers/urls.py
+++ authors/apps/followers/urls.py
@@ -5,7 +5,7 @@ from .views import ListCreateFollow, DeleteFollower
 app_name = 'followers'
 
 urlpatterns = [
-    path('follow/<username>/', DeleteFollower.as_view(), name='delete_url'),
+    path('profiles/<username>/unfollow', DeleteFollower.as_view(), name='delete_url'),
     path('profiles/<username>/follow', ListCreateFollow.as_view(),
          name='follow_url'),
     path('following/', ListCreateFollow.as_view(), name='following_url')
