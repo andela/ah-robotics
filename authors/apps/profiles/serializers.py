41a814ebb26c53ccf1324e78c52b3dd0a2450885
Paul Vitalis <vitalispaul48@live.com>
2019-02-05 15:31:47 +0300

    feat(userFollowUnfollow): add followers count
    - update profiles serializers with followers field
    - define method for getting follower count
    
    [#162949081]

--- authors/apps/profiles/serializers.py
+++ authors/apps/profiles/serializers.py
@@ -3,17 +3,19 @@ from rest_framework import serializers
 from .models import UserProfile
 from authors.apps.followers.models import Follower
 
+
 class ProfileSerializer(serializers.ModelSerializer):
     """User Profile object fields"""
     username = serializers.CharField(source='user.username',
                                      read_only=True)
     bio = serializers.CharField()
     image = serializers.ImageField(default=None)
+
     class Meta:
         model = UserProfile
         fields = ['username', 'bio', 'image',
-                'created_at', 'updated_at']
-        
+                  'created_at', 'updated_at']
+
 
 class AuthorSerializer(serializers.ModelSerializer):
     """Author object fields"""
@@ -26,6 +28,7 @@ class AuthorSerializer(serializers.ModelSerializer):
         model = UserProfile
         fields = ['username', 'bio', 'image']
 
+
 class FollowSerializer(serializers.ModelSerializer):
     """User Profile object fields"""
     username = serializers.CharField(source='user.username',
@@ -34,23 +37,29 @@ class FollowSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(default=None)
     following_count = serializers.SerializerMethodField()
     following = serializers.SerializerMethodField()
+    followers = serializers.SerializerMethodField()
 
     class Meta:
         model = UserProfile
         fields = ['username', 'bio', 'image',
-                'following', 'following_count']
+                  'following', 'following_count',
+                  'followers']
 
-    def get_following_count(self,instance):
+    def get_following_count(self, instance):
         return Follower.objects.filter(user_id=instance.user.id).count()
 
-    def get_following(self,instance):
+    def get_following(self, instance):
         request = self.context.get('request')
         user_id = request.user.id
         try:
             follower_exists = Follower.objects.filter(
-                user_id=user_id,followed_user_id=instance.user.id).first()
+                user_id=user_id, followed_user_id=instance.user.id).first()
             if follower_exists is not None:
                 return True
             return False
         except Follower.DoesNotExist:
             return False
+
+    def get_followers(self, instance):
+        return Follower.objects.filter(
+            followed_user_id=instance.user.id).count()
